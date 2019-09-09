a01ac6bebd39440510beb57febd61835f0e7118f
Ilijaa <Ilijaa@users.noreply.github.com>
2019-09-09 19:08:13 +0200

    Updates

---
+++ precache-manifest.3a74e18ea68355aac912495a82bf9b30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d852da823912dd33461fcc48bae2d2",
+    "url": "/X-Cube-Template/index.html"
+  },
+  {
+    "revision": "4adc5836a8f0af46b382",
+    "url": "/X-Cube-Template/static/css/main.077ea807.chunk.css"
+  },
+  {
+    "revision": "97c6da061a823bbfd598",
+    "url": "/X-Cube-Template/static/js/2.215d8f95.chunk.js"
+  },
+  {
+    "revision": "4adc5836a8f0af46b382",
+    "url": "/X-Cube-Template/static/js/main.b4af69b8.chunk.js"
+  },
+  {
+    "revision": "697bcbab3c9dc8030a29",
+    "url": "/X-Cube-Template/static/js/runtime~main.0404bc0b.js"
+  },
+  {
+    "revision": "6245521aeaa6f8ad1c3a9185b7cf2b2b",
+    "url": "/X-Cube-Template/static/media/arseny-togulev-DE6rYp1nAho-unsplash (1).6245521a.jpg"
+  },
+  {
+    "revision": "b035dfafda0b38c0d3c821a5352be12a",
+    "url": "/X-Cube-Template/static/media/checklist-3222079_1920.b035dfaf.jpg"
+  },
+  {
+    "revision": "7e4694e6ba8caf0cdd436ce69b206318",
+    "url": "/X-Cube-Template/static/media/online-marketing-hIgeoQjS_iE-unsplash.7e4694e6.jpg"
+  }
+]);
